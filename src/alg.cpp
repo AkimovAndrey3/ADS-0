f81b60bf25a1f6ab8d250382cb1f4fe15bfcd4fa
AkimovAndrey3 <124913657+AkimovAndrey3@users.noreply.github.com>
2023-02-18 20:49:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,12 +2,11 @@
 
 int gcd(int a, int b) {
   while (a > 0 && b > 0) {
-    if (a > b){
+    if (a > b) {
       a -= b;
-    }  
-    else{
+    } else {
       b -= a;
-    }  
+    }
   }
   return a + b;
 }
